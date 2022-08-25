5d9abab8476981ef4919c384e7d4c96499d818f0
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-08-25 06:54:30 +0300

    Added useState example.

--- react-abc/src/App.js
+++ react-abc/src/App.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [likes, setLikes] = useState(0);
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
   return (
     <div className="App">
-      <h1>Here comes new React App :)</h1>
+      <h1>{likes}</h1>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
