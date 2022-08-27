39abbd4e7ed4b96c0796f5cb2c81cb1aedb10e78
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-08-27 21:16:57 +0300

    Added separate functional component.

--- react-abc/src/App.js
+++ react-abc/src/App.js
@@ -1,23 +1,12 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  const [likes, setLikes] = useState(0);
   const [value, setValue] = useState("Input text");
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
 
   return (
     <div className="App">
-      <h2>{likes}</h2>
-
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter />
 
       <h2>{value}</h2>
       <input
