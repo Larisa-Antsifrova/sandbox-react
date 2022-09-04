b31cab20aca772ce7ce9b613454893a70c155282
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-09-04 09:42:50 +0300

    Looped throught array of posts.

--- react-abc/src/App.js
+++ react-abc/src/App.js
@@ -1,46 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import PostItem from "./components/PostItem";
 import "./styles/app.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "JavaScript",
+      description:
+        "JS - one of the programming languages, used widely in browsers.",
+    },
+    {
+      id: 2,
+      title: "CSS",
+      description: "Declarative language for styles.",
+    },
+    {
+      id: 3,
+      title: "HTML",
+      description: "Hyper Text Markup Language",
+    },
+    {
+      id: 4,
+      title: "React",
+      description: "One of the tools to build user interfaces.",
+    },
+    {
+      id: 5,
+      title: "Vue",
+      description: "The best framework to build user interfaces :p.",
+    },
+  ]);
+
   return (
     <div className="App">
-      <PostItem
-        post={{
-          id: 1,
-          title: "JavaScript",
-          description:
-            "JS - one of the programming languages, used widely in browsers.",
-        }}
-      />
-      <PostItem
-        post={{
-          id: 2,
-          title: "CSS",
-          description: "Declarative language for styles.",
-        }}
-      />
-      <PostItem
-        post={{
-          id: 3,
-          title: "HTML",
-          description: "Hyper Text Markup Language",
-        }}
-      />
-      <PostItem
-        post={{
-          id: 4,
-          title: "React",
-          description: "One of the tools to build user interfaces.",
-        }}
-      />
-      <PostItem
-        post={{
-          id: 5,
-          title: "Vue",
-          description: "The best framework to build user interfaces :p.",
-        }}
-      />
+      {posts.map(post => (
+        <PostItem post={post} key={post.id} />
+      ))}
     </div>
   );
 }
