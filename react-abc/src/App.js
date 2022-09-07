9476aea4cbde85df8fa29db0eaee57ca0d654d6c
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-09-07 06:50:19 +0300

    Moved data to separate directory.

--- react-abc/src/App.js
+++ react-abc/src/App.js
@@ -1,40 +1,14 @@
 import React, { useState } from "react";
 import PostsList from "./components/PostsList";
+import { articles } from "./data/posts";
 import "./styles/app.css";
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "JavaScript",
-      description:
-        "JS - one of the programming languages, used widely in browsers.",
-    },
-    {
-      id: 2,
-      title: "CSS",
-      description: "Declarative language for styles.",
-    },
-    {
-      id: 3,
-      title: "HTML",
-      description: "Hyper Text Markup Language",
-    },
-    {
-      id: 4,
-      title: "React",
-      description: "One of the tools to build user interfaces.",
-    },
-    {
-      id: 5,
-      title: "Vue",
-      description: "The best framework to build user interfaces :p.",
-    },
-  ]);
+  const [posts, setPosts] = useState(articles);
 
   return (
     <div className="App">
-      <PostsList title={"List of Tools"} posts={posts} />
+      <PostsList title="List of Tools" posts={posts} />
     </div>
   );
 }
---
+++ react-abc/src/data/posts.js
@@ -0,0 +1,28 @@
+export const articles = [
+  {
+    id: 1,
+    title: "JavaScript",
+    description:
+      "JS - one of the programming languages, used widely in browsers.",
+  },
+  {
+    id: 2,
+    title: "CSS",
+    description: "Declarative language for styles.",
+  },
+  {
+    id: 3,
+    title: "HTML",
+    description: "Hyper Text Markup Language",
+  },
+  {
+    id: 4,
+    title: "React",
+    description: "One of the tools to build user interfaces.",
+  },
+  {
+    id: 5,
+    title: "Vue",
+    description: "The best framework to build user interfaces :p.",
+  },
+];
