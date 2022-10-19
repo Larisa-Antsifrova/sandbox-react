45ba5d11d32af4a668c3142dc848d03a4b1b75bb
larisa.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-10-20 00:28:31 +0100

    Added navbar with links.

--- react-abc/src/App.js
+++ react-abc/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import './styles/app.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import PostsPage from "./pages/PostsPage";
 
 function App() {
 return (
 <BrowserRouter>
+    <div className="navbar">
+
+        <div className="navbar__links">
+            <Link to="/about">About</Link>
+            <Link to="/posts">Posts</Link>
+        </div>
+    </div>
+
     <Routes>
         <Route path="/about" element={<AboutPage/>}/>
         <Route path="/posts" element={<PostsPage/>}/>
