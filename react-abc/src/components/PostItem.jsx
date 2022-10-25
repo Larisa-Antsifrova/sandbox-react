875c349631a3cb05a078362fcf9d4c8c14f8a075
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-10-25 06:54:17 +0300

    Used navigation hook.

--- react-abc/src/components/PostItem.jsx
+++ react-abc/src/components/PostItem.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import ButtonBasic from "./ui/buttons/ButtonBasic";
+import { useNavigate } from "react-router-dom";
 
 const PostItem = ({ post, remove }) => {
+  const navigate = useNavigate();
+
+  const open = postId => navigate(`/posts/${postId}`);
+
   return (
     <div className="post">
       <div className="post__content">
         <p className="post__title">
-          <strong>{post.id}. {post.title}</strong>
+          <strong>
+            {post.id}. {post.title}
+          </strong>
         </p>
 
         <p>{post.body}</p>
       </div>
 
       <div className="post__btns">
+        <ButtonBasic onClick={() => open(post.id)}>Open</ButtonBasic>
         <ButtonBasic onClick={() => remove(post.id)}>Delete</ButtonBasic>
       </div>
     </div>
