ae3b86ef5437f0d1d5fd728da0d20dabb07e6066
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-10-27 22:38:25 +0300

    Added new route for post details.

--- react-abc/src/components/AppRouter.jsx
+++ react-abc/src/components/AppRouter.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import AboutPage from "../pages/AboutPage";
 import HomePage from "../pages/HomePage";
+import PostDetailsPage from "../pages/PostDetailsPage";
 import PostsPage from "../pages/PostsPage";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/about" element={<AboutPage />} />
-      <Route path="/posts" element={<PostsPage />} />
+      <Route path="/posts" exact element={<PostsPage />} />
+      <Route path="/posts/:id" exact element={<PostDetailsPage />} />
       <Route path="/" element={<HomePage />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
