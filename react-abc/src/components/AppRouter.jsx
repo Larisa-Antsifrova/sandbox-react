ce7fb88a2e42e3ab7dde8b2a81c956d6d2c6db30
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-11-07 06:41:02 +0200

    Separated private and public routes.

--- react-abc/src/components/AppRouter.jsx
+++ react-abc/src/components/AppRouter.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 
-import {router} from "../router";
+import { privateRoutes, publicRoutes } from "../router";
 
 const AppRouter = () => {
   return (
     <Routes>
-        { router.map( route =>
-        (<Route key={route.path} path={route.path} element={route.component} exact={route.exact}/>))}
+      {privateRoutes.map(route => (
+        <Route
+          key={route.path}
+          path={route.path}
+          element={route.component}
+          exact={route.exact}
+        />
+      ))}
+
+      {publicRoutes.map(route => (
+        <Route
+          key={route.path}
+          path={route.path}
+          element={route.component}
+          exact={route.exact}
+        />
+      ))}
       <Route path="/" element={<HomePage />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
--- react-abc/src/router/index.js
+++ react-abc/src/router/index.js
@@ -3,9 +3,12 @@ import PostsPage from "../pages/PostsPage";
 import PostDetailsPage from "../pages/PostDetailsPage";
 import LoginPage from "../pages/LoginPage";
 
-export const router = [
-    {path: '/about', component: <AboutPage/>, exact: true},
-    {path: '/posts', component: <PostsPage/>, exact: true},
-    {path: '/posts/:id', component: <PostDetailsPage/>, exact: true},
-    {path: '/login', component: <LoginPage/>, exact: true},
-]
+export const privateRoutes = [
+  { path: "/about", component: <AboutPage />, exact: true },
+  { path: "/posts", component: <PostsPage />, exact: true },
+  { path: "/posts/:id", component: <PostDetailsPage />, exact: true },
+];
+
+export const publicRoutes = [
+  { path: "/login", component: <LoginPage />, exact: true },
+];
