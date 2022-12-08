954ff66077f7af956800333ff7f4cd82b582085f
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-12-08 21:29:37 +0200

    Added cells to board.

--- react-chess/src/models/figures/Board.ts
+++ react-chess/src/models/figures/Board.ts
@@ -2,4 +2,20 @@ import { Cell } from './Cell'
 
 export class Board {
   cells: Cell[][] = []
+
+  public initCells(){
+    for (let i = 0; i < 8; i++) {
+      const row: Cell[] = []
+
+      for (let j = 0; j < 8; j++) {
+        if((i + j) % 2 !==0){
+          row.push(new Cell()) // Black cells
+        } else {
+          row.push(new Cell()) // White cells
+        }
+      }
+
+      this.cells.push(row)
+    }
+  }
 }
