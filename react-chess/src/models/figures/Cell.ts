fc6976aa802403cf4d112428d97168a965fe0c46
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-12-09 22:14:43 +0200

    Added cells class. Edited Color.

--- react-chess/src/models/figures/Cell.ts
+++ react-chess/src/models/figures/Cell.ts
@@ -1,1 +1,29 @@
-export class Cell {}
+import { Color } from './Color'
+import { Figure } from './Figure'
+import { Board } from './Board'
+
+export class Cell {
+  readonly x: number
+  readonly y: number
+  readonly color: Color
+  figure: Figure | null
+  board: Board
+  available: boolean
+  id: number
+
+  constructor(
+    board: Board,
+    x: number,
+    y: number,
+    color: Color,
+    figure: Figure | null
+  ) {
+    this.x = x
+    this.y = y
+    this.color = color
+    this.figure = figure
+    this.board = board
+    this.available = false
+    this.id = Math.random()
+  }
+}
