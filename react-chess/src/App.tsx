24e023b4c7965ea92c8c0f9286d32741bc3a46a5
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-12-11 23:13:04 +0200

    Rendered board.

--- react-chess/src/App.tsx
+++ react-chess/src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import BoardComponent from './components/BoardComponent'
+import { Board } from './models/figures/Board'
 
 function App() {
+  const [board, setBoard] = useState(new Board())
+
+  useEffect(()=>{
+    restart()
+  }, [])
+
+  function restart():void {
+    const newBoard = new Board()
+    newBoard.initCells()
+    setBoard(newBoard)
+  }
+
   return (
     <div className="app">
      <h1>Chess app</h1>
 
-      <BoardComponent/>
+      <BoardComponent board={board} setBoard={setBoard}/>
     </div>
   );
 }
