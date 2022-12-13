de63a2c3bc755416c6a0f36d0b5c5e81ab546d7e
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-12-13 23:46:51 +0200

    Added figures enum.

--- react-chess/src/models/figures/Figure.ts
+++ react-chess/src/models/figures/Figure.ts
@@ -1,1 +1,32 @@
-export class Figure {}
+import { Color } from './Color'
+// @ts-ignore
+import logo from '../../assets/black-bishop.png'
+import { Cell } from './Cell'
+
+export enum FigureName {
+  FIGURE = 'FIGURE',
+  KING = 'KING',
+  KNIGHT = 'KNIGHT',
+  PAWN = 'PAWN',
+  QUEEN = 'QUEEN',
+  ROOK = 'ROOK',
+  BISHOP = 'BISHOP',
+}
+
+export class Figure {
+  color: Color
+  logo: typeof logo
+  cell: Cell
+  name: FigureName
+  id: number
+
+  constructor(color: Color, cell: Cell) {
+    this.color = color
+    this.cell = cell
+    this.cell.figure = this
+    this.logo = null
+    this.name = FigureName.FIGURE
+    this.id = Math.random()
+  }
+
+}
