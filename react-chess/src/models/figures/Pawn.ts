0ab37c3735d07805e542b23812f1472e558df150
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-12-19 08:30:24 +0200

    Added constructor to pawn.

--- react-chess/src/models/figures/Pawn.ts
+++ react-chess/src/models/figures/Pawn.ts
@@ -1,3 +1,14 @@
-import { Figure } from './Figure'
+import { Figure, FigureName } from './Figure'
+import { Color } from '../Color'
+import { Cell } from '../Cell'
+import blackLogo from '../../assets/black-pawn.png'
+import whiteLogo from '../../assets/white-pawn.png'
 
-export class Pawn extends Figure {}
+export class Pawn extends Figure {
+  constructor(color: Color, cell: Cell) {
+    super(color, cell)
+
+    this.logo = color === Color.BLACK ? blackLogo : whiteLogo
+    this.name = FigureName.PAWN
+  }
+}
