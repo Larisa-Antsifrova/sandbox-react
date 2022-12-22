535b48e8174e23bff2c67387d5054f1e1733b702
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-12-22 18:16:09 +0200

    Ignored import highlights.

--- react-chess/src/models/figures/King.ts
+++ react-chess/src/models/figures/King.ts
@@ -1,7 +1,9 @@
 import { Figure, FigureName } from './Figure'
 import { Color } from '../Color'
 import { Cell } from '../Cell'
+// @ts-ignore
 import blackLogo from '../../assets/black-king.png'
+// @ts-ignore
 import whiteLogo from '../../assets/white-king.png'
 
 export class King extends Figure {
--- react-chess/src/models/figures/Pawn.ts
+++ react-chess/src/models/figures/Pawn.ts
@@ -1,7 +1,9 @@
 import { Figure, FigureName } from './Figure'
 import { Color } from '../Color'
 import { Cell } from '../Cell'
+// @ts-ignore
 import blackLogo from '../../assets/black-pawn.png'
+// @ts-ignore
 import whiteLogo from '../../assets/white-pawn.png'
 
 export class Pawn extends Figure {
