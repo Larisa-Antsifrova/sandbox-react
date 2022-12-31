ab064b555b1268449bbc14a325830b111a3374c8
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2022-12-31 18:41:11 +0200

    Added selected cell.

--- react-chess/src/components/BoardComponent.tsx
+++ react-chess/src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Board } from '../models/Board'
 import CellComponent from './CellComponent'
+import { Cell } from '../models/Cell'
 
 interface BoardProps {
   board: Board
@@ -8,11 +9,13 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
+
     return (
         <div className={'board'}>
           {board.cells.map((row, index) =>
               <React.Fragment key={index}>
-                {row.map(cell => <CellComponent cell={cell} key={cell.id}/>)}
+                {row.map(cell => <CellComponent cell={cell} key={cell.id} isSelected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}/>)}
               </React.Fragment>)
           }
         </div>
--- react-chess/src/components/CellComponent.tsx
+++ react-chess/src/components/CellComponent.tsx
@@ -3,11 +3,12 @@ import { Cell } from '../models/Cell'
 
 interface CellProps {
   cell: Cell
+  isSelected: boolean
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
+const CellComponent: FC<CellProps> = ({cell, isSelected}) => {
   return (
-    <div className={['cell', cell.color].join(' ')}>
+    <div className={['cell', cell.color, isSelected ? 'selected' : ''].join(' ')}>
       {cell.figure?.logo && <img src={cell.figure.logo} alt={cell.figure.name}/>}
       </div>
   )
