b7152c1a081d4fcaf20e7830bf57a9d7d61233ee
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2023-01-29 19:07:22 +0200

    Added players.

--- react-chess/src/App.tsx
+++ react-chess/src/App.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import './App.css';
+import './App.css'
 import BoardComponent from './components/BoardComponent'
 import { Board } from './models/Board'
+import { Player } from './models/Player'
+import { Color } from './models/Color'
 
 function App() {
   const [board, setBoard] = useState(new Board())
 
+  const [whitePlayer, setWhitePlayer] = useState(new Player(Color.WHITE))
+  const [blackPlayer, setBlackPlayer] = useState(new Player(Color.BLACK))
+
+  const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
+
   useEffect(()=>{
     restart()
+
+    setCurrentPlayer(whitePlayer)
   }, [])
 
   function restart():void {
