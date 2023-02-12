cd01106132ff2c49d058e435255361f809eda059
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2023-02-12 22:42:49 +0200

    Added timer state.

--- react-chess/src/components/Timer.tsx
+++ react-chess/src/components/Timer.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react'
+import React, { FC, useState } from 'react'
 import { Player } from '../models/Player'
 
 interface TimerProps {
@@ -7,11 +7,17 @@ currentPlayer: Player
 }
 
 const Timer: FC<TimerProps> = ({currentPlayer, restart}) => {
+  const [whiteTime, setWhiteTime] = useState(300)
+  const [blackTime, setBlackTime] = useState(300)
+
   return (
     <div>
       <button onClick={restart}>
         Restart game
       </button>
+
+      <h2>White - {whiteTime}</h2>
+      <h2>Black - {blackTime}</h2>
     </div>
   )
 }
