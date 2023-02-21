4bcbb13ed652dbc2bf9752c777951257bc9656a4
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2023-02-21 22:18:40 +0200

    Added input.

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [value, setValue] = useState('')
+    const onChange = event => setValue(event.target.value)
+
   return (
     <div>
      <h2>React Hooks</h2>
+
+        <input type="text" value={value} onChange={onChange}/>
     </div>
   );
 }
