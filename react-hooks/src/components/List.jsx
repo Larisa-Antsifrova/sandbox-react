4e0c21b3c156b7fcb239423cf80fa4b674fcce53
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2023-03-09 08:49:07 +0000

    Added refs.

--- react-hooks/src/components/List.jsx
+++ react-hooks/src/components/List.jsx
@@ -1,9 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 
 const List = () => {
     const [todos, setTodos] = useState([])
     const [page, setPage] = useState(1)
     const limit = 20
+
+    const parentRef = useRef()
+    const childRef = useRef()
 
     const fetchTodos = (page, limit)=> {
         fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}&_page=${page}`)
@@ -16,10 +19,12 @@ const List = () => {
     }, [])
 
     return (
-        <div>
+        <div ref={parentRef}>
             {todos.map(todo =>
             <div key={todo.id} style={{padding: 30, border: '2px solid tomato'}}>{todo.title}</div>
             )}
+
+            <div ref={childRef} style={{height: 20, backgroundColor: 'green'}}></div>
         </div>
     );
 };
