50386a857a71d3739496e93ebfd6c54a75810a8f
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2023-03-11 22:24:58 +0000

    Added options.

--- react-hooks/src/hooks/useScroll.js
+++ react-hooks/src/hooks/useScroll.js
@@ -3,5 +3,11 @@ import {useEffect, useRef} from "react";
 export default function useScroll(parentRef, childRef, callback){
 const observer = useRef()
 
-    useEffect(()=>{}, [])
+    useEffect(()=>{
+        const options = {
+            root: parentRef.current,
+            rootMargin: "0px",
+            threshold: 0,
+        };
+    }, [])
 }
