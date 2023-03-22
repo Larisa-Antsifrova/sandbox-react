631923e52e811102a3a3cb3a899f19cbec97ba06
l.antsifrova <larisa.antsifrova@socialtechnologies.io>
2023-03-22 21:11:14 +0200

    Completed hook.

--- react-hooks/src/hooks/useDebounce.js
+++ react-hooks/src/hooks/useDebounce.js
@@ -1,5 +1,18 @@
-import {useRef} from "react";
+import {useCallback, useRef} from "react";
 
 export default function  useDebounce(callback, delay){
     const timer = useRef()
+
+    const debouncedCallback = useCallback(()=>{
+        if(timer.current){
+            clearTimeout(timer.current)
+        }
+
+        timer.current = setTimeout(()=>{
+            callback()
+        }, delay)
+
+        return debouncedCallback
+
+    }, [callback, delay])
 }
